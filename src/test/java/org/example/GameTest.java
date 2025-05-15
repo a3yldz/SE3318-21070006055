962f6609cb21b14b37ea6efe6f92db294d48527b
Atakan YILDIZ <96147171+a3yldz@users.noreply.github.com>
2025-05-15 22:36:43 +0300

    Update GameTest.java

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -11,21 +11,17 @@ public class GameTest {
         Warrior warrior = new Warrior("Aragorn", 100, 15);
 
         game.addCharacter(warrior);
-
-        // Yalnızca assert yapılacak bir listeye erişimimiz yok ama crash olmaması testin geçmesi için yeterli
-        // Alternatif: Reflection veya karakter sayısını dönen public bir metot
     }
 
     @Test
     public void testStartGameRunsWithoutCrash() {
         Game game = new Game();
         Warrior warrior = new Warrior("Leonidas", 100, 30);
-        Mage mage = new Mage("Gandalf", 60, 25); // Kolay ölebilsin
+        Mage mage = new Mage("Gandalf", 60, 25);
 
         game.addCharacter(warrior);
         game.addCharacter(mage);
 
-        // Bu metot sadece konsol çıktısı verir, crash olmaması test için yeterlidir.
         game.startGame();
     }
 }
